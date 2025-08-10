5e9cf66d212609d8df00440a90676db38bdb8227
stevejo12 <37164804+stevejo12@users.noreply.github.com>
2025-08-10 21:28:19 +0700

    First Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,43 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+
+const backgroundUrl = "https://res.cloudinary.com/drrd7ai50/image/upload/v1754810884/Portfolio/lightmode-bg_vqgxfj.png"
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div 
+      className={styles.home_wrapper} 
+    >
+      <div
+        className={styles.home_container}
+        style={{ backgroundImage: `url(${backgroundUrl})`}}
+      >
+        {/* intro part */}
+        <div style={{ width: '50%' }}>
+          <h2 className={styles.home_header_name}>
+            Steve Vinsensius Jo
+          </h2>
+          <h1 className={styles.home_header_intro_title}>
+            Hi! I'm Steve -
+          </h1>
+          <div className={styles.home_header_description}>
+            <p>a Frontend Developer</p>
+            <p>transitioning into AI Engineering</p>
+          </div>
+          <div className={styles.home_header_overview}>
+            I design intuitive user interface and now explore how intelligent system can understand users even better
+          </div>
+          <div className={styles.home_header_buttons}>
+            <button className={`button button-primary`}>
+              Explore My Work
+            </button>
+            <button className={`button button-secondary`}>
+              View My Resume
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {/* About me */}
+        {/* footer */}
+      </div>
     </div>
   );
 }
